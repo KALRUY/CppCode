f6e6720a97cbb3d4d5343bf793f57dfca5b5eb83
KALRUY <yurlaksun@outlook.com>
2026-07-23 12:43:05 +0800

    Create 选择排序.cpp

---
+++ 选择排序.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n,a[10];
+int main(){
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        cin>>a[i];
+    }
+    for(int i=1;i<=n-1;i++){
+        for(int j=i+1;j<=n;j++){
+            if(a[j]<a[i]){
+                swap(a[i],a[j]);
+            }
+        }
+    }
+    for(int i=1;i<=n;i++){
+        cout<<a[i]<<" ";
+    }
+}
+//Time complexity: best&worst: O(n^2), Stability: Unstable.
